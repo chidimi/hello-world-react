bba3d9f7ec686709b2762587638b51a0b688c5d9
chidimi <daichi.imai@hotmail.com>
2020-10-03 20:16:27 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greets from './components/Greets';
 
 function App() {
   return (
     <div className="App">
+      <Greets name="patty" times={4}>
+        <span role="img" aria-label="rabbit">🐰</span>
+      </Greets>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Hello, World!
         </p>
         <a
           className="App-link"
@@ -21,6 +26,11 @@ function App() {
       </header>
     </div>
   );
+}
+export const example = () => {
+const name = 'patty';
+const greet = (name: string) => <p>Hello, {name || 'guest'}!</p>;
+return <div>{greet(name)}</div>;
 }
 
 export default App;
