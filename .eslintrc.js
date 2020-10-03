156e836e33278861853fdf6377275ab02e89c05c
chidimi <daichi.imai@hotmail.com>
2020-10-03 23:04:13 +0900

    setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,110 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: true,
+      es2020: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'airbnb/hooks',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:import/typescript',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:prettier/recommended',
+      'prettier',
+      'prettier/@typescript-eslint',
+      'prettier/react',
+      'prettier/standard'
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2020,
+      project: './tsconfig.eslint.json',
+      sourceType: 'module',
+      tsconfigRootDir: __dirname,
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    plugins: [
+      '@typescript-eslint',
+      'import',
+      'jsx-a11y',
+      'prettier',
+      'react',
+      'react-hooks',
+    ],
+    root: true,
+    rules: {
+      'lines-between-class-members': [
+        'error',
+        'always',
+        {
+          exceptAfterSingleLine: true,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+      ],
+      // should be rewritten as `['error', { allowAsStatement: true }]` in ESLint 7 or later
+      // SEE: https://github.com/typescript-eslint/typescript-eslint/issues/1184
+      'no-void': 'off',
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'return',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'vars': 'all',
+          'args': 'after-used',
+          'argsIgnorePattern': '_',
+          'ignoreRestSiblings': false,
+          'varsIgnorePattern': '_',
+        },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.jsx', '.tsx'],
+        },
+      ],
+      'react/jsx-props-no-spreading': [
+        'error',
+        {
+          html: 'enforce',
+          custom: 'enforce',
+          explicitSpread: 'ignore',
+        },
+      ],
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    overrides: [
+      {
+        'files': ['*.tsx'],
+        'rules': {
+          'react/prop-types': 'off',
+        },
+      },
     ],
-    "rules": {
-    }
-};
+    settings: {
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+        },
+      },
+    },
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greets from './components/Greets';
 
-function App() {
-  return (
-    <div className="App">
-      <Greets name="patty" times={4}>
-        <span role="img" aria-label="rabbit">🐰</span>
-      </Greets>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Hello, World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-export const example = () => {
-const name = 'patty';
-const greet = (name: string) => <p>Hello, {name || 'guest'}!</p>;
-return <div>{greet(name)}</div>;
-}
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.tsx</code>
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
